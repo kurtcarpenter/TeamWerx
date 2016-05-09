420164ab2b20ad1205d0324e9f1e5ea015f40d16
kurtcarpenter <kurtcarpenter@hotmail.com>
2016-05-09 17:54:58 -0400

    Initial bootstrap

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var path = require('path')
+var config = require('./config')
+// var favicon = require('serve-favicon')
+var morgan = require('morgan')
+// var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var logger = require('winston')
+var publicRoutes = require('./routes/public')
+
+var app = express()
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', publicRoutes)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.send({
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
---
+++ routes/public.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var path = require('path')
+var router = express.Router()
+
+/* GET welcome page. */
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/welcome', 'welcome.html'))
+})
+
+module.exports = router
