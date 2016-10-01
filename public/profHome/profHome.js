d2b0a87076221763e00d4b065a745e12496170df
Eric Wan <eric@partpic.com>
2016-10-01 18:52:26 -0400

    should be good to go

--- public/profHome/profHome.js
+++ public/profHome/profHome.js
@@ -8,5 +8,17 @@ profHome.config(['$routeProvider', function ($routeProvider) {
 }])
 
 profHome.controller('profHomeCtrl', function ($scope) {
-
+  $scope.classes = [ {'department': 'CS', 'number': 1100, 'section': 'A', 'semester': 'SPRING', 'year': 2016, 'admins':['a', 'b'], 'roster': ['Curry', 'Green', 'Durant', 'Thompson', 'Barbosa'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'},
+                     {'department': 'CS', 'number': 1100, 'section': 'B', 'semester': 'SPRING', 'year': 2016, 'admins':['a', 'b'], 'roster': ['DeRozan', 'Lowry', 'Biambo', 'The Raptor', 'Rob Ford'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'},
+                     {'department': 'CS', 'number': 1100, 'section': 'C', 'semester': 'FALL', 'year': 2016, 'admins':['a', 'b'], 'roster': ['Melo', 'Porzingod', 'Rose', 'Afflalo', 'Calderon'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'} ];
+  
+  $scope.showAddClass = function ($event) {
+    $mdDialog.show({
+      clickOutsideToClose: true,
+      templateUrl: 'addClassModal.html',
+      controller: 'addClassModalCtrl',
+      controllerAs: 'ctrl',
+      targetEvent: $event
+    }) 
+  }
 })
