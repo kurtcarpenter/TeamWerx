a4bfbb613953a272c144da12c3bbb6a28be9de08
Eric Wan <eric@partpic.com>
2016-10-04 16:02:29 -0400

    think it's done now forrealsies

--- public/profHome/profHome.js
+++ public/profHome/profHome.js
@@ -7,10 +7,11 @@ profHome.config(['$routeProvider', function ($routeProvider) {
   })
 }])
 
-profHome.controller('profHomeCtrl', function ($scope) {
+profHome.controller('profHomeCtrl', function ($scope, $mdDialog) {
   $scope.classes = [ {'department': 'CS', 'number': 1100, 'section': 'A', 'semester': 'SPRING', 'year': 2016, 'admins':['a', 'b'], 'roster': ['Curry', 'Green', 'Durant', 'Thompson', 'Barbosa'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'},
                      {'department': 'CS', 'number': 1100, 'section': 'B', 'semester': 'SPRING', 'year': 2016, 'admins':['a', 'b'], 'roster': ['DeRozan', 'Lowry', 'Biambo', 'The Raptor', 'Rob Ford'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'},
-                     {'department': 'CS', 'number': 1100, 'section': 'C', 'semester': 'FALL', 'year': 2016, 'admins':['a', 'b'], 'roster': ['Melo', 'Porzingod', 'Rose', 'Afflalo', 'Calderon'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'} ];
+                     {'department': 'CS', 'number': 1100, 'section': 'C', 'semester': 'FALL', 'year': 2016, 'admins':['a', 'b'], 'roster': ['Melo', 'Porzingod', 'Rose', 'Afflalo', 'Calderon'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'},
+                     {'department': 'CS', 'number': 2340, 'section': 'J', 'semester': 'SUMMER', 'year': 2016, 'admins':['a', 'b'], 'roster': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 'minGroup': 2, 'maxGroup': 3, 'matchingStrategy': 'blah'} ];
   
   $scope.showAddClass = function ($event) {
     $mdDialog.show({
@@ -20,5 +21,7 @@ profHome.controller('profHomeCtrl', function ($scope) {
       controllerAs: 'ctrl',
       targetEvent: $event
     }) 
-  }
+  };
+
+  $scope.imagePath = 'img/default_profile.jpeg';
 })
