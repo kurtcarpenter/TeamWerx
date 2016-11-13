6c05a1db3d1f5afa6ea617b0f8b81a60521544c9
Sam Marder <sam.github.@marder.co>
2016-11-13 13:31:05 -0500

    Csv upload (#44)
    * Testing out Git plus
    
    * Added CSV parsing.
    
    * Fixed some small isues with roster uploading.

--- routes/class.js
+++ routes/class.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var router = express.Router()
+var csv = require('csv')
+
 var Course = require('../models/course')
+var Student = require('../models/student')
 var logger = require('winston')
 
 var getAll = function (req, res, next) {
@@ -23,22 +26,81 @@ var getClass = function (req, res) {
   })
 }
 
+function determineColumnIndexes (csvData, columnNames) {
+  var indexes = {}
+  var columns = csvData[0]
+  for (var i = 0; i < columnNames.length; i++) {
+    var column = columnNames[i]
+    var index = columns.indexOf(column)
+    if (index === -1) {
+      return null
+    }
+    indexes[column] = index
+  }
+  return indexes
+}
+
+function importAndEnrollStudents (roster, courseId, cb) {
+  if (!roster) {
+    return cb('Please specify a CSV file.')
+  }
+  csv.parse(roster, function (err, csvData) {
+    if (err) {
+      logger.warn(err)
+      return cb('Could not parse CSV file.')
+    }
+
+    var students = []
+
+    var columnIndexes = determineColumnIndexes(csvData, ['Name', 'User ID', 'Email Address', 'Role'])
+    // First row are column names so skip it.
+    for (var i = 1; i < csvData.length; i++) {
+      var studentData = csvData[i]
+      var name = studentData[columnIndexes['Name']]
+      var userId = studentData[columnIndexes['User ID']]
+      var email = studentData[columnIndexes['Email Address']]
+      var role = studentData[columnIndexes['Role']]
+
+      if (role !== 'Student') {
+        logger.debug('Skipping row because not a student: ' + studentData)
+        continue
+      }
+
+      students = 0
+      // TODO: Handle failures in this function.
+      Student.addStudentOrAddClass(name, userId, email, courseId, function (err) {
+        if (err) {
+          return cb(err)
+        }
+      })
+      students++
+    }
+
+    return cb(null, students)
+  })
+}
+
 var addClass = function (req, res, next) {
+  var classData = req.body.class
   var params = {
-    name: req.body.name,
-    semester: req.body.semester,
-    year: req.body.year,
+    name: classData.name,
+    semester: classData.semester,
+    year: classData.year,
     professors: [req.user._id],
     roster: [],
-    minGroup: req.body.minGroup,
-    maxGroup: req.body.maxGroup
+    minGroup: classData.minGroup,
+    maxGroup: classData.maxGroup
   }
-  Course.add(params, function (err, ret) {
+  Course.add(params, function (err, course) {
     logger.info(err)
     if (err) {
       res.status(500).send()
     } else {
-      res.send(ret)
+      // All students will be enrolled after the response is returned.
+      importAndEnrollStudents(req.body.roster, course._id, function () {
+        logger.info('Finished enrolling students.')
+      })
+      res.send(course)
     }
   })
 }
